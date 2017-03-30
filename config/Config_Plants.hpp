6f616f6eef71ec04420c4bee49a0405a66989b89
Paini <paini9819@gmail.com>
2017-03-30 17:41:13 +0200

    formatierung angepasst oO

--- config/Config_Plants.hpp
+++ config/Config_Plants.hpp
@@ -1,12 +1,12 @@
 class CfgPlants {
 	class Opium {
 		amount = 5;
-        model[] = { "CUP_c_papaver_06_EP1", "CUP_c_papaver_06_EP1", "CUP_c_papaver_06_EP1", "CUP_c_papaver_07_EP1", "CUP_c_papaver_05_EP1", "CUP_c_papaver_flower03_EP1", "CUP_c_papaver_flower02_EP1" };
+		model[] = { "CUP_c_papaver_06_EP1", "CUP_c_papaver_06_EP1", "CUP_c_papaver_06_EP1", "CUP_c_papaver_07_EP1", "CUP_c_papaver_05_EP1", "CUP_c_papaver_flower03_EP1", "CUP_c_papaver_flower02_EP1" };
 		time [] = {3,3,3,3,3,3,0};
 		offset [] = {-0.2,-0.1,0,0.4,0.4,0.4,0.4};
 		zones[] = { "opium_1" };
-        item = "opium";
-        zoneSize = 30;
+		item = "opium";
+		zoneSize = 30;
 		reqItem = "opiumseed";
 	};
 };
