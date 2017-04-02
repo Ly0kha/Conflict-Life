7ccc9db8beebb5bddf439ea6f662c350de3097e2
Paini <paini9819@gmail.com>
2017-04-02 18:54:58 +0200

    komma :D

--- config/Config_Master.hpp
+++ config/Config_Master.hpp
@@ -19,7 +19,7 @@ class Life_Settings {
     /* Player Data Saving */
     save_virtualItems = true; //Save Virtual items (all sides)?
     saved_virtualItems[] = {
-		"opiumseed"
+		"opiumseed",
         "knife",
         "pickaxe",
         "fuelEmpty",
